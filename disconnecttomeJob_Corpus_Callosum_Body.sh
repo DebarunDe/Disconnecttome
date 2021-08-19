57734d4c7633c3fccda0d9a9ae2b520ed0b4f12b
DebarunDe <42978582+DebarunDe@users.noreply.github.com>
2021-08-19 15:21:29 -0400

    Add files via upload

---
+++ disconnecttomeJob_Corpus_Callosum_Body.sh
@@ -0,0 +1,16 @@
+#!/bin/bash
+#BSUB -J disconnecttomeJob
+#BSUB -o disconnecttomeJob.%J.out
+#BSUB -e disconnecttomeJob.%J.error
+
+module unload gcc
+module load gcc/10.2.0
+module load dsi-studio/2021-08
+module load python/3.6.1
+echo "modules loaded, beginning script."
+echo "___________________________________________________________________________"
+echo " "
+python3 /project/detre/debarun/disconnecttome/run_Corpus_Callosum_Body.py
+echo "___________________________________________________________________________"
+echo " "
+echo "done!"
